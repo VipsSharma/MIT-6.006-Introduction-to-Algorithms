87873a4857d42fd12f7cc2b0d69e119c8793e604
Vipul <sharma.vips5512@gmail.com>
2020-04-18 22:59:02 +0530

    Adding lec1 peak finder version 1 solution

---
+++ lec1/classProblems/peak1D.0.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+
+n = int(input("Enter number of elements : ")) 
+arr = []
+
+
+for i in range(0, n): 
+	ele = int(input()) 
+	arr.append(ele)
+
+
+for item in enumerate(arr):
+	if( item[0] == 0 ):
+		if( len(arr) > 1):
+			if( arr[1] < arr[0] ):
+				print("1D array has a Peak at "+str(item[0])+" Index, Peak is "+str(item[1]))
+				sys.exit()
+			else:
+				continue
+		else:
+			print("1D array has a Peak at "+str(item[0])+" Index, Peak is "+str(item[1]))
+			sys.exit()
+	elif( len(arr) > item[0] + 1 ):
+		if( item[1] > arr[item[0] -1] and item[1] > arr[item[0] +1] ):
+			print("1D array has a Peak at "+str(item[0])+" Index, Peak is "+str(item[1]))
+			sys.exit()
+		else:
+			continue
+	else:
+		if( item[1] > arr[item[0] -1] ):
+			print("1D array has a Peak at "+str(item[0])+" Index, Peak is "+str(item[1]))
+			sys.exit()
+		else:		
+			print("No Peak Exists in Given Array")
